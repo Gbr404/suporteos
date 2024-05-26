aaff488c21f59e72696549724aef49fc096e1d14
Gbr404 <gabrielmarianodourado@gmail.com>
2024-05-26 19:42:15 -0300

    suporteos

---
+++ suporteos/src/main/java/com/curso/services/UsersService.java
@@ -0,0 +1,25 @@
+package com.curso.services;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Service;
+
+import com.curso.repositories.UsersRepository;
+import com.curso.domains.dtos.UsersDTO;
+
+
+@Service
+public class UsersService {
+
+@Autowired
+  private UsersRepository usersRepo;
+public List<UsersDTO> findAll(){
+//retorna uma lista de UsersDTO
+return usersRepo.findAll().stream().map(obj -> new UsersDTO(obj)).collect (Collectors.toList());
+
+}
+
+}
